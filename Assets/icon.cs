6bc39de79b7eb7a01a863d3cb9f93a85e8317089
MUKILTEO\1710347 <lolfunnymanhahahaha@gmail.com>
2022-04-21 13:28:21 -0700

    icon

---
+++ Assets/icon.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+
+public class icon : MonoBehaviour
+{
+    public Sprite[] Icons;
+    private GameObject Iconject;
+    public int Icon = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Iconject = transform.GetChild(0).gameObject;
+        Iconject.GetComponent<SpriteRenderer>().sprite = Icons[Icon];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
