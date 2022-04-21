bb0d6746f0a7a877564d97129c8127794b8a6adc
MUKILTEO\1553223 <aglez0657@gmail.com>
2022-04-21 13:33:29 -0700

    arrow almost work, tutorial almpst done

--- Assets/Ileia R/Scripts/Tutorial Level/OBJECTMoveTutText.cs
+++ Assets/Ileia R/Scripts/Tutorial Level/OBJECTMoveTutText.cs
@@ -4,12 +4,11 @@ using UnityEngine;
 
 public class OBJECTMoveTutText : MonoBehaviour
 {
-    public GameObject me;
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.gameObject.tag == "COLLIDER")
         {
-            Destroy(me);
+            Destroy(this.gameObject);
         }
     }
 }
--- Assets/Ileia R/Scripts/Tutorial Level/TutorialArrow.cs
+++ Assets/Ileia R/Scripts/Tutorial Level/TutorialArrow.cs
@@ -10,6 +10,7 @@ public class TutorialArrow : MonoBehaviour
     public GameObject jose;
 
     SpriteRenderer sr;
+    MafiaTemp targetObject;
 
     private void Start()
     {
@@ -19,6 +20,8 @@ public class TutorialArrow : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        SearchNearest();
+        //points to car
         if (CarTutorialText.isInCar == false && Car.hasbeenincar == false)
         {
             Vector3 vectorToTarget = transform.position - car.transform.position;
@@ -26,13 +29,54 @@ public class TutorialArrow : MonoBehaviour
             Quaternion q = Quaternion.AngleAxis(angle, Vector3.forward);
             transform.rotation = Quaternion.Slerp(transform.rotation, q, Time.deltaTime * speed);
         }
-        if(Car.hasbeenincar == true)
+        //points to jose
+        if (Car.hasbeenincar == true && TutorialTaskMan.firstTask == false)
         {
-            //Debug.Log("HASBEENINCAR--------------------------------------------------------");
+            Vector3 vectorToTarget = transform.position - jose.transform.position;
+            float angle = Mathf.Atan2(vectorToTarget.y, vectorToTarget.x) * Mathf.Rad2Deg;
+            Quaternion q = Quaternion.AngleAxis(angle, Vector3.forward);
+            transform.rotation = Quaternion.Slerp(transform.rotation, q, Time.deltaTime * speed);
+        }
+        //points to nearest target
+        if (TutorialTaskMan.firstTaskDone == false && TutorialTaskMan.firstTask == true)
+        {
+            SearchNearest();
+            Vector3 vectorToTarget = transform.position - targetObject.transform.position;
+            float angle = Mathf.Atan2(vectorToTarget.y, vectorToTarget.x) * Mathf.Rad2Deg;
+            Quaternion q = Quaternion.AngleAxis(angle, Vector3.forward);
+            transform.rotation = Quaternion.Slerp(transform.rotation, q, Time.deltaTime * speed);
+        }
+        //points to jose after killing mafia
+        if (TutorialTaskMan.firstTaskDone == true && tutorialTriggers.enemyCount==0)
+        {
             Vector3 vectorToTarget = transform.position - jose.transform.position;
             float angle = Mathf.Atan2(vectorToTarget.y, vectorToTarget.x) * Mathf.Rad2Deg;
             Quaternion q = Quaternion.AngleAxis(angle, Vector3.forward);
             transform.rotation = Quaternion.Slerp(transform.rotation, q, Time.deltaTime * speed);
         }
     }
+
+    private void SearchNearest()
+    {
+        float distToTarget = Mathf.Infinity;
+        MafiaTemp targetEnemy = null;
+        MafiaTemp[] allEnemies = GameObject.FindObjectsOfType<MafiaTemp>();
+
+        foreach (MafiaTemp currentEnemy in allEnemies )
+        {
+            float distToEnemy = (currentEnemy.transform.position - this.transform.position).sqrMagnitude;
+            if ( distToEnemy < distToTarget)
+            {
+                distToTarget = distToEnemy;
+                targetEnemy = currentEnemy;
+                targetObject = targetEnemy;
+            }
+        }
+
+
+        //Debug.DrawLine(this.transform.position, targetEnemy.transform.position);
+        
+
+    }
+
 }
