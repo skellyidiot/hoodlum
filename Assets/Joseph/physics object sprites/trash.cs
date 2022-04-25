c0ea0af32b0832729fd15c63755aa2f55d19ad7c
MUKILTEO\1710347 <lolfunnymanhahahaha@gmail.com>
2022-04-25 11:28:02 -0700

    no message

--- Assets/Joseph/physics object sprites/trash.cs
+++ Assets/Joseph/physics object sprites/trash.cs
@@ -5,9 +5,13 @@ using UnityEngine;
 public class trash : MonoBehaviour
 {
     public enum TRASHES { napkin,cig,burger,cup,turd,magazine,ketchup,plate,sus,arm,coal,weed,shoe,powder}
-    public static Sprite[] sprites;
+    public  Sprite[] sprites = new Sprite[14];
     // Start is called before the first frame update
-    public static Sprite GetTrashSprite(TRASHES tr)
+    public void Start()
+    {
+        GetComponent<SpriteRenderer>().sprite = GetTrashSprite(GetTrash());
+    }
+    public  Sprite GetTrashSprite(TRASHES tr)
     {
         switch (tr)
         {
@@ -42,11 +46,56 @@ public class trash : MonoBehaviour
         }
         return sprites[0];
     }
-    public static TRASHES GetTrash()
+    public float GetTrashRandom(TRASHES tr)
     {
-        float rng = Random.RandomRange(0, 1);
-        print(rng);
-        return TRASHES.coal
+        switch (tr)
+        {
+            case TRASHES.napkin:
+                return 0.8f;
+            case TRASHES.cig:
+                return 0.9f;
+            case TRASHES.burger:
+                return 0.6f;
+            case TRASHES.cup:
+                return 0.5f;
+            case TRASHES.turd:
+                return 0.5f;
+            case TRASHES.magazine:
+                return 0.2f;
+            case TRASHES.ketchup:
+                return 0.5f;
+            case TRASHES.plate:
+                return 0.5f;
+            case TRASHES.sus:
+                return 0.000000000001f;
+            case TRASHES.arm:
+                return 0.2f;
+            case TRASHES.coal:
+                return 0.7f;
+            case TRASHES.weed:
+                return 0.5f;
+            case TRASHES.shoe:
+                return 0.7f;
+            case TRASHES.powder:
+                return 0.3f;
+        }
+        return 0;
+    }
 
+    public TRASHES GetTrash()
+    {
+        while (true) {
+            int rng1 = Random.Range(0, 13);
+
+            float rng = Random.Range(0f, 1f);
+            print(rng);
+            TRASHES TheTrash = (TRASHES)rng1;
+            if (rng <= GetTrashRandom((TheTrash)))
+            {
+                return TheTrash;
+            }
+            print("Didnt get it :(");
+        }
+        
     }
 }
