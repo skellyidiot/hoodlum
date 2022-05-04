8b9c954588d424ea55993a77acba21731987750a
MUKILTEO\1710347 <lolfunnymanhahahaha@gmail.com>
2022-05-04 13:39:27 -0700

    no message

---
+++ Assets/Door.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+    bool Open = false;
+    // Start is called before the first frame update
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag != "Wall")
+        {
+            GetComponent<HingeJoint2D>().useMotor = true;
+            if (Open != true)
+            {
+                GetComponent<AudioSource>().Play();
+                Open = true;
+            }
+        }
+    }
+}
