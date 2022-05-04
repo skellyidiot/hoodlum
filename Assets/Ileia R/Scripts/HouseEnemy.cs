e70cf52cb4666723b979e9dacac932e2089aaeb8
DESKTOP-T1NG7IC\ileia_ <ileia.rezaei2005@gmail.com>
2022-05-04 15:48:34 -0700

    ra

---
+++ Assets/Ileia R/Scripts/HouseEnemy.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HouseEnemy : MonoBehaviour
+{
+    [SerializeField]
+    Transform castPoint;
+
+    [SerializeField]
+    float agroRange;
+
+    private void Update()
+    {
+        if (CanSeePlayer(agroRange))
+        {
+            this.GetComponent<FollowPath>().enabled = false;
+            Debug.Log("Player in sight");
+        }
+        else
+        {
+            this.GetComponent<FollowPath>().enabled = true;
+        }
+    }
+    bool CanSeePlayer(float distance)
+    {
+        bool val = false;
+        float castDist = distance;
+
+        Vector2 endPos = castPoint.position + Vector3.right * distance;
+        RaycastHit2D hit = Physics2D.Linecast(castPoint.position, endPos, 1 << LayerMask.NameToLayer("Action"));
+
+        if(hit.collider != null)
+        {
+            if (hit.collider.gameObject.CompareTag("Player")){
+                //do stuff
+                val = true;
+                //this.GetComponent<FollowPath>().enabled = false;
+                Debug.Log("Player in sight IN F");
+            }
+            else
+            {
+                val = false;
+                Debug.Log("Nothing IN F ");
+            }
+        }
+        return val;
+    }
+}
---
+++ Assets/Ileia R/Scripts/RayCastTwoPointO.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RayCastTwoPointO : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, transform.TransformDirection(Vector2.right), 10f);
+
+        if (hit)
+        {
+            Debug.Log("Hit");
+        }
+    }
+}
