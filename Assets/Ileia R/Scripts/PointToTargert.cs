e7b7cb903803d17f24853f52a43bea35d09c63bd
E:\VGD\ILEIA <ileia.rezaei2005@gmail.com>
2022-05-04 12:54:06 -0700

    minor UI changes

--- Assets/Ileia R/Scripts/PointToTargert.cs
+++ Assets/Ileia R/Scripts/PointToTargert.cs
@@ -7,6 +7,7 @@ public class PointToTargert : MonoBehaviour
     public float speed = 2f;
     public SpriteRenderer sr;
     //task 1 objects
+    public GameObject safe;
     public GameObject GoIn;
     //task 2 objects
     public GameObject Leader;
@@ -27,7 +28,7 @@ public class PointToTargert : MonoBehaviour
         {
             sr.enabled = false;
         }
-        if(TaskmanTXTbox.doingTask1 == true && TaskmanTXTbox.doneTask1 == false)
+        if (TaskmanTXTbox.doingTask1 == true && TaskmanTXTbox.doneTask1 == false && AllTasks.isInBuilding == false)
         {
             sr.enabled = true;
             Vector3 vectorToTarget = transform.position - GoIn.transform.position;
@@ -35,10 +36,17 @@ public class PointToTargert : MonoBehaviour
             Quaternion q = Quaternion.AngleAxis(angle, Vector3.forward);
             transform.rotation = Quaternion.Slerp(transform.rotation, q, Time.deltaTime * speed);
         }
-        if (TaskmanTXTbox.doingTask1 == true && TaskmanTXTbox.doneTask1 == false && AllTasks.isInBuilding == true)
+        if (AllTasks.isInBuilding == true && AllTasks.hasInfo == false)
+        {
+            sr.enabled = true;
+            Vector3 vectorToTarget = transform.position - safe.transform.position;
+            float angle = Mathf.Atan2(vectorToTarget.y, vectorToTarget.x) * Mathf.Rad2Deg;
+            Quaternion q = Quaternion.AngleAxis(angle, Vector3.forward);
+            transform.rotation = Quaternion.Slerp(transform.rotation, q, Time.deltaTime * speed);
+        }
+        if(AllTasks.hasInfo == true)
         {
             sr.enabled = false;
-            Debug.Log(AllTasks.isInBuilding);
         }
 
         if (TaskmanTXTbox.doingTask2 == true && TaskmanTXTbox.doneTask2 == false && AllTasks.LeaderInCar == false)
@@ -49,6 +57,7 @@ public class PointToTargert : MonoBehaviour
             Quaternion q = Quaternion.AngleAxis(angle, Vector3.forward);
             transform.rotation = Quaternion.Slerp(transform.rotation, q, Time.deltaTime * speed);
         }
+        
         if (TaskmanTXTbox.doingTask2 == true && TaskmanTXTbox.doneTask2 == false && AllTasks.LeaderInCar == true)
         {
             sr.enabled = true;
