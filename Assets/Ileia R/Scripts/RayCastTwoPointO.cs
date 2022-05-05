7ef5275e01df555c6a428ca150c67b70b0ad158c
E:\VGD\ILEIA <ileia.rezaei2005@gmail.com>
2022-05-05 13:39:11 -0700

    enemy

--- Assets/Ileia R/Scripts/RayCastTwoPointO.cs
+++ Assets/Ileia R/Scripts/RayCastTwoPointO.cs
@@ -4,6 +4,13 @@ using UnityEngine;
 
 public class RayCastTwoPointO : MonoBehaviour
 {
+    public LayerMask layer;
+
+    public Transform firePoint;
+    public GameObject bulletprefab;
+    public GameObject muzzle;
+
+    public float force = 20f;
 
     // Start is called before the first frame update
     void Start()
@@ -14,11 +21,31 @@ public class RayCastTwoPointO : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        RaycastHit2D hit = Physics2D.Raycast(transform.position, transform.TransformDirection(Vector2.right), 10f);
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, transform.up, 7f);
 
         if (hit)
         {
-            Debug.Log("Hit");
+            Debug.DrawRay(transform.position, transform.TransformDirection(Vector2.up)* 7f, Color.green);
+            Debug.Log(hit.collider.gameObject.tag);
+            if (hit.collider.gameObject.tag == "Player")
+            {
+                gameObject.GetComponent<FollowPath>().enabled = false;
+                Debug.Log(hit.collider.gameObject.tag);
+                Shoot();
+            }
+            else
+            {
+                gameObject.GetComponent<FollowPath>().enabled = true;
+            }
         }
+        
+    }
+    
+    void Shoot()
+    {
+        GameObject bullet = Instantiate(bulletprefab, firePoint.position, firePoint.rotation);
+        Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>();
+        rb.AddForce(firePoint.up * force, ForceMode2D.Impulse);
+        Debug.Log("Shooting");
     }
 }
