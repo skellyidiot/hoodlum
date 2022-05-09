191e1c87340ad91f606e2a8e5d2e336e5e946ca5
MUKILTEO\1710347 <lolfunnymanhahahaha@gmail.com>
2022-05-09 12:04:32 -0700

    no message

---
+++ Assets/Safe.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Safe : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        AllTasks.TheTaskSctipt.infosteal = this.gameObject;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
