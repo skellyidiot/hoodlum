c1d7c9fc9edfed1899eea8e57267505bcf530251
Qualpo <74394798+Qualpo@users.noreply.github.com>
2022-05-09 16:39:35 -0700

    no message

---
+++ Assets/Music.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Music : MonoBehaviour
+{
+    static AudioSource Ass;
+
+    private void Start()
+    {
+        
+        Ass = GetComponent<AudioSource>();
+    }
+    // Start is called before the first frame update
+    public static void ChangeMusic(AudioClip music)
+    {
+        if (Ass.clip != music)
+        {
+            Ass.clip = music;
+            Ass.Play();
+        }
+    }
+}
